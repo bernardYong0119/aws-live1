a6a5e1cb315dcac076f226f72129b6e767720ff1
bernardYong0119 <132746337+bernardYong0119@users.noreply.github.com>
2023-05-08 13:42:31 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "assdatabase.cnqjbll5yosk.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "assdatabase"
-custombucket = "tanjieyi-bucket"
-customregion = "us-east-1"
-
