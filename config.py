38b673ae4f57eae7a3761820d2a4f0754e83e75d
bernardYong0119 <132746337+bernardYong0119@users.noreply.github.com>
2023-05-08 21:19:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "assdatabase.cnqjbll5yosk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "assDatabase"
+custombucket = "tanjieyi-bucket"
+customregion = "us-east-1"
+
